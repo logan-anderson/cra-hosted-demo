3748462bb03dae74ae96926f692a85850fcbeb5a
Logan Anderson <landerson2@upei.ca>
2020-06-23 11:34:42 -0300

    Updates

---
+++ precache-manifest.07c86a2053d04c57f5499eefe28bf012.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f609403547393c13f221d43a8bd378f3",
+    "url": "/cra-hosted-demo/index.html"
+  },
+  {
+    "revision": "729ee0ddc6dcaac94427",
+    "url": "/cra-hosted-demo/static/css/main.78f5bc2a.chunk.css"
+  },
+  {
+    "revision": "ca0f469a0ca23d9aa111",
+    "url": "/cra-hosted-demo/static/js/2.681dd941.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cra-hosted-demo/static/js/2.681dd941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "729ee0ddc6dcaac94427",
+    "url": "/cra-hosted-demo/static/js/main.a09929ce.chunk.js"
+  },
+  {
+    "revision": "d404bfe8d4c81d2b90ea",
+    "url": "/cra-hosted-demo/static/js/runtime-main.ec08c906.js"
+  }
+]);
